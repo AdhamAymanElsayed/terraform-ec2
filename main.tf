e42dc73deb515c38f853dc288aa734515afe168c
Adham Ayman <120693599+AdhamAymanElsayed@users.noreply.github.com>
2024-09-25 14:57:55 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "us-east-1"  # Change to your desired region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0531a8de51635a5c8"  
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "KiwiInstance"
+  }
+}
